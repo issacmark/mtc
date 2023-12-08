5d804463ab061c410bb9b974564c387de9ed0420
root <huizhouma@126.com>
2023-12-08 17:04:39 +0800

    split

--- getinfo.ps1
+++ getinfo.ps1
@@ -1,16 +1,31 @@
-$regex = @"
-CB Check browser_patching_(?<browser>\w+)_(?<collection>\w+)_1:WIN\d+
-"@
+function ExtractBrowserAndCollection($text) {
+  # Split the string into parts using underscores
+  $parts = $text.Split('_')
 
-$input = "CB Check browser_patching_chrome_CO_1:WIN000091"
+  # Extract the browser type
+  $browserType = $parts[4]
 
-$match = $regex.Match($input)
+  # Extract the collection name
+  $collectionName = $parts[5]
 
-if ($match.Success) {
-    $browser = $match.Groups["browser"].Value
-    $collection = $match.Groups["collection"].Value
-    Write-Host "Browser: $browser"
-    Write-Host "Collection: $collection"
-} else {
-    Write-Host "Invalid input format."
+  # Combine the collection name parts if necessary
+  if ($collectionName.Length -eq 3) {
+    $collectionName = $collectionName[0] + "_" + $collectionName[1] + $collectionName[2]
+  }
+
+  # Return a tuple containing the extracted information
+  return @{
+    BrowserType = $browserType
+    CollectionName = $collectionName
+  }
 }
+
+# Example usage
+$text1 = "CB Check browser_patching_chrome_CO_1:WIN000091"
+$text2 = "CB Check browser_patching_edge_CS_1:WIN000092"
+
+$result1 = ExtractBrowserAndCollection($text1)
+$result2 = ExtractBrowserAndCollection($text2)
+
+Write-Host "Text 1: Browser type: $($result1.BrowserType), Collection name: $($result1.CollectionName)"
+Write-Host "Text 2: Browser type: $($result2.BrowserType), Collection name: $($result2.CollectionName)"
