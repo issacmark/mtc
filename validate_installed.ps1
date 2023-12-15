8c3d02a2bfe650b2dbde08b17cbe5d98bc28d924
root <huizhouma@126.com>
2023-12-15 10:30:16 +0800

    dd

--- validate_installed.ps1
+++ validate_installed.ps1
@@ -4,3 +4,22 @@ if ($chromeInstallDir) {
 } else {
     Write-Host "Chrome is not installed."
 }
+
+
+
+# Uninstall Google Chrome
+$chrome = Get-WmiObject -Query "Select * from Win32_Product Where (Name like 'Google Chrome%')" 
+if ($chrome) {
+    $chrome.Uninstall()
+    Write-Output "Google Chrome has been uninstalled."
+} else {
+    Write-Output "Google Chrome is not installed."
+}
+
+# Verify the uninstallation
+$chrome_check = Get-WmiObject -Query "Select * from Win32_Product Where (Name like 'Google Chrome%')"
+if (-not $chrome_check) {
+    Write-Output "Verified: Google Chrome has been successfully uninstalled."
+} else {
+    Write-Output "Google Chrome is still installed."
+}
